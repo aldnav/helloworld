9d7b1d5a3d693481ea55fcd9819275b8e6c3cef1
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-04-24 06:52:02 +0800

    Update tut_requests.py

--- tut_requests.py
+++ tut_requests.py
@@ -1,7 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-#~ soup = BeautifulSoup(html_doc)
-#~ url = 'http://cebu.mynimo.com/jobs/search/?q={0}&searchType=jobs&page=1'.format(keyword)
 
 def search_by_keyword(keyword='java'):
 	url = 'http://www.pythonforbeginners.com'
@@ -10,8 +8,20 @@ def search_by_keyword(keyword='java'):
 	soup = BeautifulSoup(data)
 	for link in soup.find_all('a'):
 		print link.get('href')
-	#~ r = requests.post('http://cebu.mynimo.com/jobs/search/?q={0}&searchType=jobs&page=1'.format(keyword))
-	#~ return r
+	r = requests.post('http://cebu.mynimo.com/jobs/search/?q={0}&searchType=jobs&page=1'.format(keyword))
+	return r
 
-result = search_by_keyword('python')
-print result
+#~ result = search_by_keyword('python')
+#~ print result
+
+start_url = requests.get('http://cebu.mynimo.com/jobs/search/?q=java&searchType=jobs&page=1')
+soup = BeautifulSoup(start_url.content)
+results = soup.find_all('a', {'class':'jobTitleLink'})
+jobtitles = []
+for result in results:
+	jobtitles.append(result.getText())
+results = soup.find_all('td', {'class':'search_highlight'})
+locations = []
+for result in results:
+	locations.append(result.getText())
+print locations
