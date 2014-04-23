585aa00efec323f762485a3eaba3b5d8db72bfa1
Aldrin Navarro <aldrinnavarro16@gmail.com>
2014-04-23 18:01:09 +0800

    Create vigenere_aldrin.py

---
+++ vigenere_aldrin.py
@@ -0,0 +1,114 @@
+import cProfile
+
+class Vigenere(object):
+	__alphabet = 'abcdefghijklmnopqrstuvwxyz1234567890 '
+	__keyword = 'excalibur'
+	__plain_text = 'now is the time for all good men to come to the aid of their fellow man'
+	__unwanted_chars = '`~!@#$%^&*()_+-={}|[]\\|;\'\"<>?,./'
+	
+	def __init__(self):
+		self.alphabet = self.__alphabet
+	
+	def encrypt(self, key, message):
+		self.__check_duplicates(key)
+		self.__check_valid_input(key)
+		self.__check_valid_input(message)
+		message = message.lower()
+		key = key.lower()
+		print 'PASSWORD : ', key
+		print '   MESSAGE : ', message
+		self.mixed_alphabet = self.__construct_mixed_alphabet(key)
+		self.table = self.__construct_table(self.mixed_alphabet)
+		count = 0
+		max_count = len(key) - 1
+		cypher = ''
+		for char in message:
+			cypher += self.table[ key[count] ][ self.alphabet.index(char) ]
+			if count < max_count:
+				count += 1
+			else:
+				count = 0
+		print '      CYPHER : ', cypher
+		print
+		return cypher
+	
+	def decrypt(self, key, message):
+		self.__check_duplicates(key)
+		self.__check_valid_input(key)
+		self.__check_valid_input(message)
+		message = message.lower()
+		key = key.lower()
+		print 'PASSWORD : ', key
+		print '   MESSAGE : ', message
+		self.mixed_alphabet = self.__construct_mixed_alphabet(key)
+		self.table = self.__construct_table(self.mixed_alphabet)
+		count = 0
+		max_count = len(key) - 1
+		cypher = ''
+		for char in message:
+			index = self.table[key[count]].index(char)
+			cypher += self.alphabet[index]
+			if count < max_count:
+				count += 1
+			else:
+				count = 0
+		print '      CYPHER : ', cypher
+		return cypher
+	
+	def __check_valid_input(self, input):		
+		for char in input:
+			count = self.__unwanted_chars.count(char)
+			if count > 0:
+				raise Exception('Message only allows alphanumeric characters.')
+	
+	def __check_duplicates(self, key):
+		for char in key:
+			count = key.count(char)
+			if count > 1:
+				raise Exception('Password/Key cannot contain duplicate characters')
+			
+	def __construct_mixed_alphabet(self, key):
+		string = self.alphabet.translate(None, key)
+		string = key + string
+		count = 0
+		flag = 5
+		max_passes = flag - 1
+		passes = 0
+		mixed = ''
+		
+		while len(mixed) < len(self.alphabet) and passes <= max_passes:
+			for char in string:
+				if count % flag == passes:
+					mixed += char
+				count += 1
+			passes += 1
+			count = 0
+		return mixed
+	
+	def __construct_table(self, alphabet):
+		r = c = 0
+		dim = len(alphabet)
+		mat = {}
+		row = ''
+		for r_symbol in alphabet:
+			for c_symbol in alphabet:
+				row += alphabet[c]				
+				c += 1
+				if c >= dim: c = 0
+			mat[self.__alphabet[r]] = row
+			row = ''
+			r += 1
+			c = r % dim
+		return mat
+	
+if __name__ == '__main__':
+	krypto = Vigenere()
+	
+	message = 'now is the time for all good men to come to the aid of their fellow man'
+	password = 'excalibur'
+	
+	cProfile.run("encrypted = krypto.encrypt(password, message)")
+	cProfile.run("decrypted = krypto.decrypt(password, encrypted)")
+	
+	print encrypted
+	print decrypted
